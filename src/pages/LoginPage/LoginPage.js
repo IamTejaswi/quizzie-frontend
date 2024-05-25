50878224c67b0c45977fc61dad42fe30728072c3
Bharghava-Oruganti <obharghava@gmail.com>
2024-05-25 11:59:11 +0530

    ADDED Modal and Tab components

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Login from '../../components/Login/Login'
+import OptionList from '../../components/shared/inputTabs/Tabs'
+import Modal from '../../components/shared/inputTabs/modal/Modal'
+
 export default function LoginPage() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleModalOpen = () => {
+    setIsModalOpen(true)
+  }
+
+  const handleModalClose = () => {
+    setIsModalOpen(false)
+  }
   return (
     <div>
-      <Login/>
+ 
+      <button type="button" onClick={handleModalOpen}>
+                Click Me to Open Modal
+      </button>
+      {
+        isModalOpen &&
+        <Modal
+            setIsOpen={setIsModalOpen}>
+            <>
+          <Login /> 
+              <OptionList />
+            </>
+        </Modal>
+        
+      }
+            {/* <Modal setIsOpen={setIsModalOpen}>
+                <>
+                        <Login />
+                        <OptionList />
+                </>
+            </Modal> */}
     </div>
   )
 }
